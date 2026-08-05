91bfefa3da9d7a922db3eeae5b1da4146ecda518
feanhorse-ship-it <feanhorse@gmail.com>
2026-08-05 21:34:14 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,26 @@
 import Hero from "@/components/Hero";
 import NewsSection from "@/components/NewsSection";
-import MatchSection from "@/components/MatchSection";
+import NextMatchSection from "@/components/NextMatchSection";
 import SquadPreview from "@/components/SquadPreview";
+import GalleryPreview from "@/components/GalleryPreview";
+import PartnersSection from "@/components/PartnersSection";
 
 export default function Home() {
   return (
-    <>
+    <main className="bg-[#071B3B] text-white">
+
       <Hero />
+
       <NewsSection />
-      <MatchSection />
+
+      <NextMatchSection />
+
       <SquadPreview />
-    </>
+
+      <GalleryPreview />
+
+      <PartnersSection />
+
+    </main>
   );
 }
