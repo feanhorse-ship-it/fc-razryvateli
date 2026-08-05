df4ea28e94528aac20228ae21c41ed87c21a8b04
feanhorse-ship-it <feanhorse@gmail.com>
2026-08-05 16:41:41 +0300

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,27 @@
+export default function Footer(){
+
+return(
+
+<footer className="bg-[#06152d] py-14 mt-24">
+
+<div className="container">
+
+<h2 className="text-3xl font-black">
+
+ФК РАЗРЫВАТЕЛИ
+
+</h2>
+
+<p className="mt-4 text-slate-400">
+
+© 2026 Все права защищены.
+
+</p>
+
+</div>
+
+</footer>
+
+);
+
+}
