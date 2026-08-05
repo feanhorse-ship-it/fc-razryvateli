099075363744acb7297a2f1d913c359973759fe9
feanhorse-ship-it <feanhorse@gmail.com>
2026-08-05 16:41:32 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import NewsSection from "@/components/NewsSection";
+import MatchSection from "@/components/MatchSection";
+import SquadPreview from "@/components/SquadPreview";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <NewsSection />
+      <MatchSection />
+      <SquadPreview />
+    </>
+  );
+}
