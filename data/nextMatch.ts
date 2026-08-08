8b499d46000dfea144d02bacb400b03fd144902e
feanhorse-ship-it <feanhorse@gmail.com>
2026-08-08 14:38:58 +0300

    Create nextMatch.ts

---
+++ data/nextMatch.ts
@@ -0,0 +1,15 @@
+export interface NextMatch {
+  opponent: string;
+  date: string;
+  time: string;
+  stadium: string;
+  poster: string;
+}
+
+export const nextMatch: NextMatch = {
+  opponent: "Динамо",
+  date: "10 августа 2026",
+  time: "18:00",
+  stadium: "Петроградец",
+  poster: "/images/match-poster.png",
+};
